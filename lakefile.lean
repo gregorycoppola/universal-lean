f091f56d088fbfd96a9e984ac0fabb485bc81bcb
Greg <greg@coppola.ai>
2026-03-05 13:37:55 -0500

    📦 Updated 1 file
    Modified:
      - akefile.lean

--- lakefile.lean
+++ lakefile.lean
@@ -2,10 +2,7 @@ import Lake
 open Lake DSL
 
 package «universal-lean» where
-  name := "universal-lean"
-
-require mathlib from git
-  "https://github.com/leanprover-community/mathlib4.git"
+  name := `universalLean
 
 lean_lib «UniversalLean» where
   roots := #[`UniversalLean]
